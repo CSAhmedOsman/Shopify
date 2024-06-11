cf9527b5436752e7fc00b1164e5d5e9d8c995471
anas <hello.anas07@gmail.com>
2024-06-12 01:59:40 +0300

    make desgin for fav Screen

--- SwiftCart/AppDelegate.swift
+++ SwiftCart/AppDelegate.swift
@@ -13,7 +13,7 @@ import FirebaseAuth
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWdequeueithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
     
         FirebaseApp.configure()
