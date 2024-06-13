6146fa32ec44ddc984b75a06f332beec0722e563
anas <hello.anas07@gmail.com>
2024-06-13 20:05:30 +0300

    merge into Dev

--- SwiftCart/AppDelegate.swift
+++ SwiftCart/AppDelegate.swift
@@ -9,13 +9,18 @@ import UIKit
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-    
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.resignOnTouchOutside = true
+        
         FirebaseApp.configure()
         return true
     }
